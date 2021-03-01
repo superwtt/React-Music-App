8ce390b6825475bc3d1ee6a59aa577b5f75100ac
superwtt <keepgowtt@126.com>
2021-03-01 09:41:00 +0800

    新增路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Switch, Route, Router, HashRouter, Link } from "react-router-dom";
-import Recommend from '../pages/recommend';
-import Singer from '../pages/singer';
+import Recommend from "../pages/recommend";
+import Singer from "../pages/singer";
 
-const RouterConfig = {
-  
+export default class RouterConfig extends React.Component {
+  render() {
+    return (
+      <div>
+        <HashRouter forceRefresh={true}>
+          <Switch>
+            <Route path="/recommend" component={Recommend}></Route>
+            <Route path="/Singer" component={Singer}></Route>
+          </Switch>
+        </HashRouter>
+      </div>
+    );
+  }
 }
-
-export default RouterConfig;
-
