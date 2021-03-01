7e4a8e9b61a5ded22c6fbcabf65150983b672bc8
superwtt <keepgowtt@126.com>
2021-03-01 14:19:56 +0800

    新增路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { Switch, Route, Router, HashRouter, Link } from "react-router-dom";
+import { Switch, Route, Router, HashRouter, Redirect } from "react-router-dom";
 import Recommend from "../pages/recommend";
 import Singer from "../pages/singer";
 
 export default class RouterConfig extends React.Component {
   render() {
     return (
-      <div>
         <HashRouter forceRefresh={true}>
           <Switch>
+            
             <Route path="/recommend" component={Recommend}></Route>
-            <Route path="/Singer" component={Singer}></Route>
+            <Route path="/singer" component={Singer}></Route>
+            <Redirect from="/" to="/recommend" /> 
+            {/* Redirect必须放Switch里的最后一行，如果上面的路由都匹配不到，跳转到“/”页面，即渲染的Recommend组件 */}
           </Switch>
         </HashRouter>
-      </div>
     );
   }
 }
