9a704ada9535aabb0c5f1519b91fbd752fad7448
superwtt <keepgowtt@126.com>
2021-03-02 17:37:54 +0800

    热门歌单样式

--- src/pages/recommend/index.js
+++ src/pages/recommend/index.js
@@ -1,10 +1,28 @@
 import React from "react";
-import Slider from '@/common/component/slider';
+import Slider from "@/common/component/slider";
+
+import './index.less';
 
 const Recommend = () => {
-  return <div>
-    <Slider />
-  </div>;
+  return (
+    <div>
+      <Slider />
+      <div className="recommendList">
+        <h1 className="listTitle">热门歌单推荐</h1>
+        <ul>
+          <li className="item">
+            <div className="icon">
+              <img width="60" height="60" src="http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/3687967.jpg" />
+            </div>
+            <div className="text">
+              <h2 className="name"></h2>
+              <p className="desc"></p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Recommend;
