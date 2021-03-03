c9b246bb697c1409a270279b78341d6ede7622e4
superwtt <keepgowtt@126.com>
2021-03-03 11:58:31 +0800

    热门歌单列表

--- src/pages/recommend/index.js
+++ src/pages/recommend/index.js
@@ -12,11 +12,38 @@ const Recommend = () => {
         <ul>
           <li className="item">
             <div className="icon">
-              <img width="60" height="60" src="http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/3687967.jpg" />
+              <img width="60" height="60" src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1" />
             </div>
             <div className="text">
-              <h2 className="name"></h2>
-              <p className="desc"></p>
+              <h2 className="name">孟冬</h2>
+              <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
+            </div>
+          </li>
+          <li className="item">
+            <div className="icon">
+              <img width="60" height="60" src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1" />
+            </div>
+            <div className="text">
+              <h2 className="name">孟冬</h2>
+              <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
+            </div>
+          </li>
+          <li className="item">
+            <div className="icon">
+              <img width="60" height="60" src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1" />
+            </div>
+            <div className="text">
+              <h2 className="name">孟冬</h2>
+              <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
+            </div>
+          </li>
+          <li className="item">
+            <div className="icon">
+              <img width="60" height="60" src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1" />
+            </div>
+            <div className="text">
+              <h2 className="name">孟冬</h2>
+              <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
             </div>
           </li>
         </ul>
