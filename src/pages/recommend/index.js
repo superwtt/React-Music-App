57d83111ae0cbe14be89964333b7719f4c3724d1
superwtt <keepgowtt@126.com>
2021-03-03 16:20:32 +0800

    修改首页滚动点样式

--- src/pages/recommend/index.js
+++ src/pages/recommend/index.js
@@ -7,113 +7,114 @@ import "./index.less";
 const Recommend = () => {
   return (
     <div className="recommend">
-      <div className="recommendContent">
+      <Scroll>
+        <div className="">
           <Slider />
-
-        <div className="recommendList">
-          <h1 className="listTitle">热门歌单推荐</h1>
-          <ul>
-            <li className="item">
-              <div className="icon">
-                <img
-                  width="60"
-                  height="60"
-                  src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1"
-                  alt=""
-                />
-              </div>
-              <div className="text">
-                <h2 className="name">孟冬</h2>
-                <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
-              </div>
-            </li>
-            <li className="item">
-              <div className="icon">
-                <img
-                  width="60"
-                  height="60"
-                  src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1"
-                  alt=""
-                />
-              </div>
-              <div className="text">
-                <h2 className="name">孟冬</h2>
-                <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
-              </div>
-            </li>
-            <li className="item">
-              <div className="icon">
-                <img
-                  width="60"
-                  height="60"
-                  src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1"
-                  alt=""
-                />
-              </div>
-              <div className="text">
-                <h2 className="name">孟冬</h2>
-                <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
-              </div>
-            </li>
-            <li className="item">
-              <div className="icon">
-                <img
-                  width="60"
-                  height="60"
-                  src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1"
-                  alt=""
-                />
-              </div>
-              <div className="text">
-                <h2 className="name">孟冬</h2>
-                <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
-              </div>
-            </li>
-            <li className="item">
-              <div className="icon">
-                <img
-                  width="60"
-                  height="60"
-                  src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1"
-                  alt=""
-                />
-              </div>
-              <div className="text">
-                <h2 className="name">孟冬</h2>
-                <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
-              </div>
-            </li>
-            <li className="item">
-              <div className="icon">
-                <img
-                  width="60"
-                  height="60"
-                  src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1"
-                  alt=""
-                />
-              </div>
-              <div className="text">
-                <h2 className="name">孟冬</h2>
-                <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
-              </div>
-            </li>
-            <li className="item">
-              <div className="icon">
-                <img
-                  width="60"
-                  height="60"
-                  src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1"
-                  alt=""
-                />
-              </div>
-              <div className="text">
-                <h2 className="name">孟冬</h2>
-                <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
-              </div>
-            </li>
-          </ul>
+          <div className="recommendList">
+            <h1 className="listTitle">热门歌单推荐</h1>
+            <ul>
+              <li className="item">
+                <div className="icon">
+                  <img
+                    width="60"
+                    height="60"
+                    src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1"
+                    alt=""
+                  />
+                </div>
+                <div className="text">
+                  <h2 className="name">孟冬</h2>
+                  <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
+                </div>
+              </li>
+              <li className="item">
+                <div className="icon">
+                  <img
+                    width="60"
+                    height="60"
+                    src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1"
+                    alt=""
+                  />
+                </div>
+                <div className="text">
+                  <h2 className="name">孟冬</h2>
+                  <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
+                </div>
+              </li>
+              <li className="item">
+                <div className="icon">
+                  <img
+                    width="60"
+                    height="60"
+                    src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1"
+                    alt=""
+                  />
+                </div>
+                <div className="text">
+                  <h2 className="name">孟冬</h2>
+                  <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
+                </div>
+              </li>
+              <li className="item">
+                <div className="icon">
+                  <img
+                    width="60"
+                    height="60"
+                    src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1"
+                    alt=""
+                  />
+                </div>
+                <div className="text">
+                  <h2 className="name">孟冬</h2>
+                  <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
+                </div>
+              </li>
+              <li className="item">
+                <div className="icon">
+                  <img
+                    width="60"
+                    height="60"
+                    src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1"
+                    alt=""
+                  />
+                </div>
+                <div className="text">
+                  <h2 className="name">孟冬</h2>
+                  <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
+                </div>
+              </li>
+              <li className="item">
+                <div className="icon">
+                  <img
+                    width="60"
+                    height="60"
+                    src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1"
+                    alt=""
+                  />
+                </div>
+                <div className="text">
+                  <h2 className="name">孟冬</h2>
+                  <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
+                </div>
+              </li>
+              <li className="item">
+                <div className="icon">
+                  <img
+                    width="60"
+                    height="60"
+                    src="http://qpic.y.qq.com/music_cover/JKNRKMiciaU6Su0ibZV93emzptlPMwSbiaksya7Ck7lcAvtk4QPR2YtSQw/300?n=1"
+                    alt=""
+                  />
+                </div>
+                <div className="text">
+                  <h2 className="name">孟冬</h2>
+                  <p className="desc">温柔公子张真源，磁性嗓音诱你</p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+      </Scroll>
     </div>
   );
 };
