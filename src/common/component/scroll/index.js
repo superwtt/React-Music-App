da335a9c9a7f1448716ce1fc77ca8c1575cef904
superwtt <keepgowtt@126.com>
2021-03-04 09:47:41 +0800

    better-scroll 滚动

--- src/common/component/scroll/index.js
+++ src/common/component/scroll/index.js
@@ -1,19 +1,13 @@
 import React, { useEffect } from "react";
 
-import Swiper from "swiper";
-import "swiper/dist/css/swiper.min.css";
+import BScroll from 'better-scroll';
 
 const Scroll = (props) => {
 
   const initScroll = () => {
-
-    new Swiper(".swiper-bar", {
-      scrollbar: ".swiper-scrollbar",
-      direction: "vertical",
-      slidesPerView: "auto",
-      mousewheelControl: true,
-      freeMode: true,
-      roundLengths: true, //防止文字模糊
+    const wrapper = document.querySelector('wrapper')
+    new BScroll(wrapper, {
+      probeType: 1,
     });
 
   };
@@ -23,16 +17,11 @@ const Scroll = (props) => {
   }, []);
 
   return (
-    <div className="swiper-bar" style={{height:'100%'}}>
-      <div className="swiper-wrapper">
-        <div className="swiper-slide" >
-          {
-            props.children
-          }
-        </div>
-      </div>
-      <div className="swiper-scrollbar"></div>
-    </div>
+   <div className="wrapper">
+     {
+       props.children
+     }
+   </div>
   );
 };
 
