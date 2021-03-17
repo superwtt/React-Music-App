498ef98aa437a1995b649e14af7dc41005990df2
superwtt <keepgowtt@126.com>
2021-03-17 19:20:58 +0800

    歌手列表数据格式化

--- src/pages/singer/index.js
+++ src/pages/singer/index.js
@@ -1,13 +1,64 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+
+import SingerJS from "@/common/js/singer";
 import * as actionCreators from "./store/actionCreators";
 
 import "./index.less";
 
+const HOT_NAME = "热门";
+const HOT_SINGER_LEN = 10;
+
 const Singer = (props) => {
+  const _normalizeSinger = (list) => {
+    let map = {
+      hot: {
+        title: HOT_NAME,
+        items: [],
+      },
+    };
+    list.forEach((item, index) => {
+      if (index < HOT_SINGER_LEN) {
+        map.hot.items.push(new SingerJS({
+          name: item.Fsinger_name,
+          id: item.Fsinger_mid,
+        }));
+      }
+      const key = item.Findex;
+      if (!map[key]) {
+        map[key] = {
+          title: key,
+          items: [],
+        };
+      }
+      map[key].items.push(
+        new SingerJS({
+          name: item.Fsinger_name,
+          id: item.Fsinger_mid,
+        })
+      );
+    });
+    // 为了得到有序列表，我们需要处理 map
+    let ret = []
+    let hot = []
+    for (let key in map) {
+      let val = map[key]
+      if (val.title.match(/[a-zA-Z]/)) {
+        ret.push(val)
+      } else if (val.title === HOT_NAME) {
+        hot.push(val)
+      }
+    }
+    ret.sort((a, b) => {
+      return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+    })
+    return hot.concat(ret)
+  };
 
   useEffect(() => {
-    props.getSingerList();
+    props.getSingerList().then((res) => {
+      console.log(_normalizeSinger(res.data.list));
+    });
   }, []);
 
   return <div className="singer">Singer page</div>;
@@ -19,8 +70,8 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    getSingerList(){
-      dispatch(actionCreators.getSingerList())
+    getSingerList() {
+     return dispatch(actionCreators.getSingerList());
     },
   };
 };
