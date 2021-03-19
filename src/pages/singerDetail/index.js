9f6bc68025edf62958bdec855c812353c933160f
superwtt <keepgowtt@126.com>
2021-03-19 14:04:16 +0800

    歌手页面子路由跳转

--- src/pages/singerDetail/index.js
+++ src/pages/singerDetail/index.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 import "./index.less";
 
-const SingerDetail = ()=>{
-    return <div className="singerDetail"></div>
-}
+const SingerDetail = (props) => {
 
-export default SingerDetail
+    console.log(props.location.pathname)
+  
+  return (
+    <TransitionGroup>
+      <CSSTransition
+        key={props.location.pathname}
+        timeout={10000}
+        classNames="app2"
+      >
+        <div className="singerDetail">1</div>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
+
+export default SingerDetail;
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { Switch, Route, HashRouter, Redirect } from "react-router-dom";
+
 import Recommend from "../pages/recommend";
 import Singer from "../pages/singer";
 import Rank from "../pages/rank";
 import Search from "../pages/search";
 import SingerDetail from "../pages/singerDetail";
 
-export default class RouterConfig extends React.Component {
-  render() {
-    return (
-      <HashRouter forceRefresh={true}>
-        <Switch>
-          <Route path="/recommend" component={Recommend} />
-          <Route exact path="/singer" component={Singer}/>
-          <Route path="/singer/:id" component={SingerDetail} />
-          <Route path="/rank" component={Rank} />
-          <Route path="/search" component={Search} />
-          <Redirect from="/" to="/recommend" />
-          {/* Redirect必须放Switch里的最后一行，如果上面的路由都匹配不到，跳转到“/”页面，即渲染的Recommend组件 */}
-        </Switch>
-      </HashRouter>
-    );
-  }
-}
+const RouterConfig = () => {
+  return (
+    <HashRouter forceRefresh={true}>
+      <Switch>
+        <Route path="/recommend" component={Recommend} />
+        <Route exact path="/singer" component={Singer} />
+        <Route path="/singer/:id" component={SingerDetail} />
+        <Route path="/rank" component={Rank} />
+        <Route path="/search" component={Search} />
+        <Redirect from="/" to="/recommend" />
+        {/* Redirect必须放Switch里的最后一行，如果上面的路由都匹配不到，跳转到“/”页面，即渲染的Recommend组件 */}
+      </Switch>
+    </HashRouter>
+  );
+};
+export default RouterConfig;
