4db2bb7687053ef1a3e21ae0873c510f127b142d
superwtt <keepgowtt@126.com>
2021-03-20 09:55:27 +0800

    歌单列表滚动

--- src/pages/musicList/index.js
+++ src/pages/musicList/index.js
@@ -1,25 +1,42 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
 
 import Scroll from "@/common/component/scroll";
 import SongList from "@/common/component/songList";
 import "./index.less";
 
+const RESERVED_HEIGHT = 40;
+let imageHeightGlobal = 0;
 
 const MusicList = (props) => {
+
+  const [scrollY,setScrollY] = useState(0);
+
   const { songs } = props;
 
   const musicList = useRef(null);
 
+  const scroll = (pos)=>{
+    setScrollY(pos.y)
+  }
+
   useEffect(() => {
-      const imageHeight = document.getElementsByClassName("bgImage")[0].clientHeight;
-      const list = document.getElementsByClassName("list")[0];
+    const imageHeight = document.getElementsByClassName("bgImage")[0].clientHeight;
+    const list = document.getElementsByClassName("list")[0];
 
-      console.log(list)
-      console.log(imageHeight)
+    imageHeightGlobal=imageHeight;
 
-      list.style.top = `${imageHeight}px`;
+    console.log(list);
+    console.log(imageHeight);
+
+    list.style.top = `${imageHeight}px`;
   }, []);
+
+  useEffect(()=>{
+    const translateY = Math.max(-imageHeightGlobal+RESERVED_HEIGHT,scrollY);
+    const bgLayer = document.getElementsByClassName("bgLayer")[0];
+    bgLayer.style['transform'] = `translate3d(0,${translateY}px,0)`
+  },[scrollY])
 
   return (
     <div className="musicList">
@@ -27,14 +44,20 @@ const MusicList = (props) => {
         <i className="icon-back iconBack"></i>
       </div>
       <h1 className="title">{props.title}</h1>
-      <div 
+      <div
         className="bgImage"
         style={{ backgroundImage: "url(" + props.bgImage + ")" }}
       >
         <div className="filter"></div>
       </div>
-      
-      <Scroll data={songs} classVal={"list"} ref={musicList}>
+      <div className="bgLayer"></div>
+      <Scroll 
+       listenScroll={true}
+       probeType={3}
+       scroll={scroll}
+       data={songs} 
+       classVal={"list"} 
+       ref={musicList}>
         <div className="songListWrapper">
           <SongList songs={songs} />
         </div>
