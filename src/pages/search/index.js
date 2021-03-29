309846dd6799bcc49b8fde0ec32ccf00dc377295
superwtt <keepgowtt@126.com>
2021-03-29 18:15:28 +0800

    search-box组件映射到页面

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import SearchBox from "@/common/component/searchBox";
+import './index.styl';
+
 const Search = () => {
-  return <div>Search page</div>;
+  return <div className="search">
+    <div className="search-box-wrapper">
+      <SearchBox />
+    </div>
+  </div>;
 };
 
 export default Search;
