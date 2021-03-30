732dfcab7dd33f669981d40b91a83c25a16c276b
superwtt <keepgowtt@126.com>
2021-03-30 12:05:50 +0800

    热门搜索

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import { getHotKey } from "@/services/search";
 import SearchBox from "@/common/component/searchBox";
-import './index.styl';
+import "./index.less";
 
-const Search = () => {
-  return <div className="search">
-    <div className="search-box-wrapper">
-      <SearchBox />
+const Search = (props) => {
+  const [hotKey, setHotKey] = useState([]);
+
+  useEffect(() => {
+    getHotKey().then((res) => {
+      const {
+        data: { hotkey },
+      } = res;
+      setHotKey(hotkey.slice(0, 10));
+    });
+  }, []);
+
+  return (
+    <div className="search">
+      <div className="search-box-wrapper">
+        <SearchBox />
+      </div>
+      <div className="shortcut-wrapper">
+        <div className="shortcut">
+          <div className="hot-key">
+            <h1 className="title">热门搜索</h1>
+            <ul>
+              {hotKey.map((item, index) => {
+                return (
+                  <li key={index} className="item">
+                    <span>{item.k}</span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Search;
